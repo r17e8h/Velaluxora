7eaa6562ef9a8c8645e7961432ca460428270751
r17e8h <riteshkr.rana18@gmail.com>
2026-04-24 23:29:15 +0530

    final fix

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.options('*', cors());
 app.use(cors({
   origin: 'https://velaluxora.vercel.app',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
