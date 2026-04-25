2cc28a22d97db8d5ad626f0c998f7dac0e1c0c76
r17e8h <riteshkr.rana18@gmail.com>
2026-04-25 14:01:58 +0530

    fix: ui for admin panel on phones and fix deployment issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { CartProvider } from './context/CartContext'
 import { AuthProvider } from './context/AuthContext.jsx';
 import axios from 'axios';
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "";
 axios.defaults.withCredentials = true;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
